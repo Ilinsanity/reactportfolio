ed547f8380a28213728eb95de8db0f6b22baac6f
Ilinsanity <Issaclin2002@gmail.com>
2024-01-15 02:29:05 -0500

    cleaning up the css

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import React, { useState } from "react";
 function App() {
   const [screen, setscreen] = useState({ top: 0, left: 0 });
   return (
-    <div className="w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="w-screen h-screen bg-mountainrange bg-cover bg-center">
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <p className="text-8xl name text-white">Issac</p>
+        <p className="text-8xl name text-white">Lin</p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        antdark: "url('/src/images/ant-darkened2.jpg')",
+        mountainrange: "url('/src/images/mount.JPG')",
+      },
+    },
   },
   plugins: [],
 };
