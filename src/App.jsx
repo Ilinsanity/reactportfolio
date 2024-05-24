e200029ee767662881f70fed63e28f204c2e5ef4
Issac Lin <issaclin2002@gmail.com>
2024-05-24 23:10:28 +0800

    made change to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import React, { useState } from "react";
 
+import React, { useState } from "react";
+import Aboutme from "./aboutme.jsx";
 function App() {
   const [screen, setscreen] = useState({ top: 0, left: 0 });
   return (
@@ -8,12 +9,12 @@ function App() {
       <div className="w-screen h-screen flex items-center justify-end bg-mountainwosky bg-cover bg-center absolute z-30 top-0 ">
         <div className="w-max h-max mr-5 flex flex-col">
           <button className="navname">
-            <h1 className=" uppercase text-gold text-4xl rounded p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-black">
+            <h1 className=" uppercase text-gold text-4xl rounded p-4 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-black">
               About Me
             </h1>
           </button>
           <button className="navname">
-            <h1 className=" uppercase text-gold text-4xl rounded p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-black">
+            <h1 className=" uppercase text-gold text-4xl rounded p-4 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-black">
               Projects
             </h1>
           </button>
@@ -26,7 +27,7 @@ function App() {
       </div>
 
       <div className="w-screen h-screen bg-mountainrange bg-cover bg-center absolute z-10 top-0 bg-fixed"></div>
-      <div className="w-screen h-screen bg-white"></div>
+      <Aboutme />
     </div>
   );
 }
--- src/aboutme.jsx
+++ src/aboutme.jsx
@@ -1,6 +1,6 @@
 import "./aboutme.css";
 function aboutme() {
-  return <div></div>;
+  return <div className="w-screen h-screen bg-slate-600"></div>;
 }
 
 export default aboutme;
